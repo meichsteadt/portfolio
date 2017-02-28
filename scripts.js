d7532fc9f9ac16e88386a02551d3fdf74a5bcd25
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-02-28 02:20:30 -0800

    start styling portfolio

---
+++ scripts.js
@@ -0,0 +1,41 @@
+$(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      $('#navbar').fadeIn(500);
+    }
+    else {
+      $('#navbar').fadeOut(500);
+    }
+  });
+	// Cache the Window object
+	var $window = $(window);
+	// Parallax Backgrounds
+	// Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
+
+	$('div[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+
+		$(window).scroll(function() {
+
+			// Scroll the background at var speed
+			// the yPos is a negative value because we're scrolling it UP!
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });
+
+      //Darkens the background on scroll down
+      $('.background').css('filter', "brightness(" + (50 - (($window.scrollTop()/$window.height())*15)) + '%)');
+
+      var contentScroll = ($window.scrollTop()/$window.height())*($window.height()/3);
+      if( contentScroll < 300)
+      $('.content').css('margin-top', contentScroll + 'px')
+      console.log($('.content').css('margin-top'));
+		}); // end window scroll
+	});
+
+  //Show nav on scroll
+});
